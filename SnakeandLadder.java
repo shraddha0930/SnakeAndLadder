40d7bfa32ad0212f2fc82edcb36f03360db3c073
shraddha0930 <shraddhaingole11@gmail.com>
2022-12-21 08:51:15 +0530

    initialize start position

---
+++ SnakeandLadder.java
@@ -0,0 +1,11 @@
+package day4Practiceproblem;
+
+import java.util.Random;
+
+public class SnakeandLadder {
+    public static void main(String[] args) {
+        int start_position = 0;
+        System.out.println("Start position is : " +start_position);
+
+    }
+}
