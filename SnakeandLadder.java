288404787320a7499dce5bb3e791039d7feef20a
shraddha0930 <shraddhaingole11@gmail.com>
2022-12-21 08:53:32 +0530

    player rolldie

--- SnakeandLadder.java
+++ SnakeandLadder.java
@@ -6,6 +6,21 @@ public class SnakeandLadder {
     public static void main(String[] args) {
         int start_position = 0;
         System.out.println("Start position is : " +start_position);
+        Dice();
 
     }
+    public static int Dice() {
+        int min = 1;
+        int max = 7;
+
+        Random r = new Random();
+        int Dice = r.nextInt(max);
+        if (Dice == 0) {
+            System.out.println("Invalid, please try again");
+        } else {
+            System.out.println(Dice);
+        }
+        return Dice;
+    }
+
 }
